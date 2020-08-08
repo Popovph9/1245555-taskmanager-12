9fa92a2475fcd7a711fa3a33cb59cec50cd7fd0b
philipp <popovph9@gmil.com>
2020-08-08 12:17:03 +0300

    выполнено задание 6 учебного проекта: шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import {createSiteMenuTemplate} from "./view/site-menu.js";
 import {createSiteFilterTemplate} from "./view/site-filter.js";
 import {createBoardElement} from "./view/board-element.js";
 import {createBoardFiltersTemplate} from "./view/board-filters.js";
-import {editCardTemplate} from "./view/edit-card";
-import {createCardTemplate} from "./view/create-card";
-import {createloadButtonTemplate} from "./view/load-button";
+import {editCardTemplate} from "./view/edit-card.js";
+import {createCardTemplate} from "./view/create-card.js";
+import {createloadButtonTemplate} from "./view/load-button.js";
+import {generateTask} from "./view/task.js";
+import {generateFilter} from "./view/filters.js";
 
-const CARDS_COUNT = 3;
+const CARDS_COUNT = 22;
+const CARDS_COUNT_PER_STEP = 8;
+
+const tasks = new Array(CARDS_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -17,7 +23,7 @@ const siteHeader = mainElement.querySelector(`.main__control`);
 
 render(siteHeader, createSiteMenuTemplate(), `beforeend`);
 
-render(mainElement, createSiteFilterTemplate(), `beforeend`);
+render(mainElement, createSiteFilterTemplate(filters), `beforeend`);
 render(mainElement, createBoardElement(), `beforeend`);
 
 const boardElement = mainElement.querySelector(`.board`);
@@ -25,10 +31,30 @@ const filtersContainer = boardElement.querySelector(`.board__filter-list`);
 const cardsContainer = boardElement.querySelector(`.board__tasks`);
 
 render(filtersContainer, createBoardFiltersTemplate(), `beforeend`);
-render(cardsContainer, editCardTemplate(), `beforeend`);
+render(cardsContainer, editCardTemplate(tasks[0]), `beforeend`);
 
-for (let i = 0; i < CARDS_COUNT; i++) {
-  render(cardsContainer, createCardTemplate(), `beforeend`);
+for (let i = 1; i < Math.min(tasks.length, CARDS_COUNT_PER_STEP); i++) {
+  render(cardsContainer, createCardTemplate(tasks[i]), `beforeend`);
 }
 
-render(boardElement, createloadButtonTemplate(), `beforeend`);
+if (tasks.length >= CARDS_COUNT_PER_STEP) {
+  let renderedCardsCount = CARDS_COUNT_PER_STEP;
+
+  render(boardElement, createloadButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+
+    tasks.
+      slice(renderedCardsCount, renderedCardsCount + CARDS_COUNT_PER_STEP).
+      forEach((task) => render(cardsContainer, createCardTemplate(task), `beforeend`));
+  });
+
+  renderedCardsCount += CARDS_COUNT_PER_STEP;
+
+  if (renderedCardsCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+}
---
+++ src/util.js
@@ -0,0 +1,43 @@
+export const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const getCurrentday = () => {
+  const currentDate = new Date();
+
+  currentDate.setHours(23, 59, 59, 999);
+
+  return new Date(currentDate);
+};
+
+export const isExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentday();
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
+
+export const isTaskExpireToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  const currentDate = getCurrentday();
+
+  return currentDate.getTime() === dueDate.getTime();
+};
+
+export const isRepeating = (repeating) => {
+  return Object.values(repeating).some(Boolean);
+};
+
+export const humanizeTaskDueDate = (dueDate) => {
+  return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
+};
